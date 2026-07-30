0b7e5643e0e05f944817477b9f435b24776353fc
Kanaga Subramanian <kanaga@genome.com>
2026-07-30 08:04:52 +0530

    FirstReact app

---
+++ react-app/src/component/Counter.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function Counter (){
+    const [count,setCount]=useState(0)
+    const element = React.createElement("div",null,React.createElement("h1",null,"title"),React.createElement("p",null,"paragrapg"))
+//     return (
+//   <>
+//      <div>
+//          <h1>Title</h1>
+//         <p>Paragraph</p>
+//    </div>
+//    <div>
+//          <h1>Title</h1>
+//         <p>Paragraph</p>
+//    </div>
+//   </>
+//     )
+    return <>
+
+    <h1>{count}</h1>
+    <button onClick={
+       ()=>{
+         setCount(count+1)
+       }
+    }>Count ++</button>
+    </>
+}
+
+export default Counter
